d422e568747496d0d91097436b4d194afd0f55e4
rosenkrans <mark.rosenkrans@gmail.com>
2019-07-20 15:00:50 -0400

    added handle methods to single budget

--- client/src/components/SingleBudget.js
+++ client/src/components/SingleBudget.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Redirect, Link } from 'react-router-dom'
 
 export default class SingleBudget extends Component {
+
+    state= {
+        budget: {},
+        isEditFormDisplayed: false, 
+        redirectToHome: false
+    }
+
+    componentDidMount() {
+        axios.get(`/api/budgets/${this.props.match.params.budgetId}`)
+            .then((res) => {
+                this.setState({budget: res.data})
+            })
+    }
+
+    handleInputChange = (event) => {
+        const copiedBudget = {...this.state.budget}
+        copiedBudget[event.target.name] = event.target.value
+
+        this.setState({budget: copiedBudget}) 
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        axios.put(`/api/budgets/${this.state.budget._id}`, this.state.budget)
+            .then((res) => {
+                this.setState({
+                    budget: res.data,
+                    isEditFormDisplayed: false 
+                })
+            })
+    }
+
+    handleToggleEditForm = () => {
+        this.setState((state) => {
+            return {isEditFormDisplayed: !state.isEditFormDisplayed}
+        })
+    }
+        
+    handleDeleteBudget = () => {
+        axios.delete(`/api/budgets/${this.state.budget._id}`)
+            .then(() => {
+                this.setState({redirectToHome: true})
+            })
+    }
+
     render() {
         return (
             <div>
@@ -9,3 +57,4 @@ export default class SingleBudget extends Component {
         )
     }
 }
+
